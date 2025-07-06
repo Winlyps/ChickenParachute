875d2583caebb461b377cd99f81bde95eb1d35be
Haxlyst <Haxlyst@gmail.com>
2025-07-06 20:06:50 +0200

    Initial commit

---
+++ src/main/kotlin/haxlyst/chickenParachute/ChickenParachute.kt
@@ -0,0 +1,14 @@
+package haxlyst.chickenParachute
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class ChickenParachute : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
